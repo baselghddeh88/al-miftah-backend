3d977dbeb01d5e69a2c88fb5567eb2415e1ab0a6
Basel Bhddeh <sam@Basels-MacBook.local>
2026-04-30 16:30:25 -0700

    fix: seed in background

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ php artisan cache:clear
 echo "🗄️ Running Database Migrations..."
 sleep 5
 php artisan migrate --force --no-interaction
-
+php artisan db:seed --class=SyriaRegionsSeeder --force --no-interaction &
 echo "✅ App is ready!"
 
 exec php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
