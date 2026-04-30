75ad7e57cb882b17417780a1a316e99fe6ae21c9
Basel Bhddeh <sam@Basels-MacBook.local>
2026-04-30 15:54:02 -0700

    fix: remove seed

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,13 @@ set -e
 
 echo "🚀 Starting App Initialization..."
 
-# تنظيف الكاش
 php artisan config:clear
 php artisan cache:clear
 
-echo "📂 Running Database Migrations..."
-# الانتظار قليلاً للتأكد من أن قاعدة البيانات جاهزة
+echo "🗄️ Running Database Migrations..."
 sleep 5
 php artisan migrate --force --no-interaction
-php artisan db:seed --force --no-interaction
+
 echo "✅ App is ready!"
 
-# تشغيل سيرفر لارافيل بدلاً من Apache
 exec php artisan serve --host=0.0.0.0 --port=8000
